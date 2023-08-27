de5df59e8d28c44c34b0ba47ca1ecb85683ec36d
wasfa-shahid <128019171+wasfa-shahid@users.noreply.github.com>
2023-08-27 19:47:46 +0500

    Add files via upload

--- Profile.js
+++ Profile.js
@@ -1,64 +1,62 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-    import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
-document.addEventListener('DOMContentLoaded', function () {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js"; // Import initializeApp
+import { getAuth, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
 
-    
-    
-    
-    
-      // Your web app's Firebase configuration
-      const firebaseConfig = {
-        apiKey: "AIzaSyDmlL7LNlg4ZAYQhtEXyydjn3pNH8We-9U",
-        authDomain: "blog-707f4.firebaseapp.com",
-        projectId: "blog-707f4",
-        storageBucket: "blog-707f4.appspot.com",
-        messagingSenderId: "1068783077281",
-        appId: "1:1068783077281:web:967a29d2f5ab88afbcade0"
-      };
-    
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    
-    // Get a reference to the Firebase Realtime Database
-    const database = getDatabase(app);
+const firebaseConfig = {
+    apiKey: "AIzaSyDmlL7LNlg4ZAYQhtEXyydjn3pNH8We-9U",
+    authDomain: "blog-707f4.firebaseapp.com",
+    projectId: "blog-707f4",
+    storageBucket: "blog-707f4.appspot.com",
+    messagingSenderId: "1068783077281",
+    appId: "1:1068783077281:web:967a29d2f5ab88afbcade0"
+  };
 
-    // Simulate caching username and uid
-    var cachedUsername = localStorage.getItem('u.firstName') // Replace with your actual cached username
-    var cachedUid = localStorage.getItem('u.id') // Replace with your actual cached UID
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); // Initialize Firebase Authentication
 
-    // Get references to form elements
-    var usernameField = document.getElementById('username');
-    var currentPasswordField = document.getElementById('currentPassword');
-    var newPasswordField = document.getElementById('newPassword');
-    var confirmPasswordField = document.getElementById('confirmPassword');
-    var changePasswordButton = document.getElementById('changePasswordButton');
+var cachedUsername = localStorage.getItem('u.firstName'); // Replace with your actual cached username
+var cachedUid = localStorage.getItem('u.id'); // Replace with your actual cached UID
 
-    // Populate the username field with cached data
-    usernameField.value = cachedUsername;
+// Get references to form elements
+var usernameField = document.getElementById('username');
+var currentPasswordField = document.getElementById('currentPassword');
+var newPasswordField = document.getElementById('newPassword');
+var confirmPasswordField = document.getElementById('confirmPassword');
+var changePasswordButton = document.getElementById('changePasswordButton');
 
-    // Handle button click
-    changePasswordButton.addEventListener('click', function () {
-        var currentPassword = currentPasswordField.value;
-        var newPassword = newPasswordField.value;
-        var confirmPassword = confirmPasswordField.value;
+// Populate the username field with cached data
+usernameField.value = cachedUsername;
 
-        // Check if new password and confirm password match
-        if (newPassword !== confirmPassword) {
-            alert('New password and confirm password do not match.');
-            return;
-        }
+changePasswordButton.addEventListener('click', function () {
+  var currentPassword = currentPasswordField.value;
+  var newPassword = newPasswordField.value;
+  var confirmPassword = confirmPasswordField.value;
 
-        // Here, you would typically send a request to your server to handle database operations securely,
-        // but for this example, we'll simulate it with a console log.
-        console.log("Simulating password change for UID: " + cachedUid);
-        console.log("Current Password: " + currentPassword);
-        console.log("New Password: " + newPassword);
+  // Check if new password and confirm password match
+  if (newPassword !== confirmPassword) {
+    alert('New password and confirm password do not match.');
+    return;
+  }
 
-        // You can then use Firebase SDK to update the password securely.
-        // firebase.auth().currentUser.updatePassword(newPassword).then(function() {
-        //     alert('Password updated successfully.');
-        // }).catch(function(error) {
-        //     alert('Error updating password: ' + error.message);
-        // });
+  // Reauthenticate the user using their email and password
+  var user = auth.currentUser;
+  var credential = EmailAuthProvider.credential(cachedUid, currentPassword);
+
+  reauthenticateWithCredential(user, credential)
+    .then(function () {
+      // User successfully reauthenticated, now update the password
+      updatePassword(user, newPassword)
+        .then(function () {
+          alert('Password updated successfully.');
+          // You can also clear the password fields if needed
+          currentPasswordField.value = "";
+          newPasswordField.value = "";
+          confirmPasswordField.value = "";
+        })
+        .catch(function (error) {
+          alert('Error updating password: ' + error.message);
+        });
+    })
+    .catch(function (error) {
+      alert('Error reauthenticating user: ' + error.message);
     });
 });
